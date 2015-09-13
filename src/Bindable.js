df44abdcc7905220ba901235ea8c6a519b36d4b2
Alexey <alexey.solonets@gmail.com>
2015-09-13 17:56:35 +0500

    initial commit

---
+++ src/Bindable.js
@@ -0,0 +1,69 @@
+﻿/**
+https://www.sencha.com/forum/showthread.php?297559-How-to-use-nested-ViewModel
+*/
+Ext.define('Ext.ux.mixin.Bindable', {
+    initBindable: function () {
+        var me = this;
+        Ext.mixin.Bindable.prototype.initBindable.apply(me, arguments);
+        me.applyInitialPublishedState();
+    },
+
+    /**
+    Notifying parent ViewModel about state changes
+    */
+    publishState: function (property, value) {
+        var me = this,
+            vm = me.lookupViewModel(),
+            parentVm = me.lookupViewModel(true),
+            path = me.viewModelKey,
+            state;
+
+        if (path && property && parentVm) {
+            path += '.' + property;
+            parentVm.set(path, value);
+        }
+
+        Ext.mixin.Bindable.prototype.publishState.apply(me, arguments);
+
+        if (property && vm && vm.getView() == me) {
+            vm.set(property, value);
+        }
+    },
+
+    /**
+    Getting published state
+    */
+    getInitialPublishedState: function () {
+        var me = this,
+            state = me.publishedState || (me.publishedState = {}),
+            publishes = me.getPublishes();
+
+        for (name in publishes) {
+            if (state[name] === undefined) {
+                state[name] = me[name];
+            }
+        }
+
+        return state;
+    },
+
+    /**
+    Applying published state to own ViewModel
+    */
+    applyInitialPublishedState: function () {
+        var me = this,
+            vm = me.lookupViewModel(),
+            state;
+
+        if (vm && vm.getView() == me) {
+            state = me.getInitialPublishedState();
+            vm.set(state);
+        }
+    }
+}, function () {
+    Ext.Array.each([Ext.Component, Ext.Widget], function (Class) {
+        Class.prototype.initBindable = Ext.ux.mixin.Bindable.prototype.initBindable;
+        Class.prototype.publishState = Ext.ux.mixin.Bindable.prototype.publishState;
+        Class.mixin([Ext.ux.mixin.Bindable]);
+    });
+});
---
+++ src/SplitViewModel.js
@@ -0,0 +1,198 @@
+﻿/**
+An override to be able split ViewModel data by ViewModel instances
+*/
+Ext.define('Ext.ux.app.SplitViewModel', {
+    override: 'Ext.app.ViewModel',
+
+    config: {
+        /**
+        @cfg {String}
+        ViewModel name
+        */
+        name: undefined,
+
+        /**
+        @cfg {String}
+        @private
+        name + sequential identifer
+        */
+        uniqueName: undefined,
+
+        /**
+        @cfg {String}
+        @private
+        id + nameDelimiter
+        */
+        prefix: undefined
+    },
+
+    nameDelimiter: '|',
+    expressionRe: /^(?:\{[!]?(?:(\d+)|([a-z_][\w\-\.|]*))\})$/i,
+    uniqueNameRe: /-\d+$/, 
+
+    privates: {
+        applyData: function (newData, data) {
+            newData = this.getPrefixedData(newData);
+            data = this.getPrefixedData(data);
+
+            return this.callParent([newData, data]);
+        },
+
+        applyLinks: function (links) {
+            links = this.getPrefixedData(links);
+            return this.callParent([links]);
+        },
+
+        applyFormulas: function (formulas) {
+            formulas = this.getPrefixedData(formulas);
+            return this.callParent([formulas]);
+        },
+
+        bindExpression: function (path, callback, scope, options) {
+            path = this.getPrefixedPath(path);
+            return this.callParent([path, callback, scope, options]);
+        }
+    },
+
+    bind: function (descriptor, callback, scope, options) {
+        if (Ext.isString(descriptor)) {
+            descriptor = this.getPrefixedDescriptor(descriptor);
+        }
+        return this.callParent([descriptor, callback, scope, options]);
+    },
+
+    linkTo: function (key, reference) {
+        key = this.getPrefixed(key);
+        return this.callParent([key, reference]);
+    },
+
+    get: function (path) {
+        path = this.getPrefixedPath(path);
+        return this.callParent([path]);
+    },
+
+    set: function (path, value) {
+        if (Ext.isString(path)) {
+            path = this.getPrefixed(path);
+        }
+        else if (Ext.isObject(path)) {
+            path = this.getPrefixedData(path);
+        }
+        this.callParent([path, value]);
+    },
+
+    applyName: function (name) {
+        name = name || this.type || 'viewmodel';
+        return name;
+    },
+
+    applyUniqueName: function (id) {
+        id = id || Ext.id(null, this.getName() + '-');
+        return id;
+    },
+
+    applyPrefix: function (prefix) {
+        prefix = prefix || this.getUniqueName() + this.nameDelimiter;
+        return prefix;
+    },
+
+    /**
+    Apply prefix to property names
+    */
+    getPrefixedData: function (data) {
+        var name, newName, value,
+            result = {};
+
+        if (!data) {
+            return null;
+        }
+
+        for (name in data) {
+            value = data[name];
+            newName = this.getPrefixed(name);
+            result[newName] = value;
+        }
+
+        return result;
+    },
+
+    /**
+    Add prefix to a string
+    */
+    getPrefixed: function (name) {
+        var prefix = this.getPrefix();
+        var result = name.indexOf(this.nameDelimiter) != -1 ? name : prefix + name;
+        return result;
+    },
+
+    /**
+    Get descriptor with a correct prefix
+    */
+    getPrefixedDescriptor: function (descriptor) {
+        var descriptorParts = this.expressionRe.exec(descriptor);
+
+        if (!descriptorParts) {
+            return descriptor;
+        }
+
+        var path = descriptorParts[2]; // '{foo}' -> 'foo'
+        descriptor = descriptor.replace(path, this.getPrefixedPath(path));
+
+        return descriptor;
+    },
+
+    /**
+    Get path with a correct prefix
+    */
+    getPrefixedPath: function (path) {
+        var nameDelimiterPos = path.lastIndexOf(this.nameDelimiter),
+            hasName = nameDelimiterPos != -1,
+            name,
+            isUnique,
+            vmUniqueName,
+            vm;
+
+        if (hasName) {
+            // bind to a ViewModel by name: viewmodel|foo.bar
+            name = path.substring(0, nameDelimiterPos + this.nameDelimiter.length - 1);
+            isUnique = this.uniqueNameRe.test(name);
+
+            if (!isUnique) {
+                // replace name by uniqueName: viewmodel-123|foo.bar
+                vm = this.findViewModelByName(name);
+                if (vm) {
+                    vmUniqueName = vm.getUniqueName();
+                    path = vmUniqueName + path.substring(nameDelimiterPos);
+                }
+                else {
+                    Ext.log({ level: 'warn' }, 'Cannot find a ViewModel instance by specifed name/type: ' + name);
+                }
+            }
+        }
+        else {
+            // bind to this ViewModel: foo.bar -> viewmodel-123|foo.bar
+            path = this.getPrefixed(path);            
+        }
+
+        return path;
+    },
+
+    /**
+    Find a ViewModel by name up by hierarchy
+    @param {String} name ViewModel's name
+    @param {Boolean} skipThis Pass true to ignore this instance
+    */
+    findViewModelByName: function (name, skipThis) {
+        var result,
+            vm = skipThis ? this.getParent() : this;
+
+        while (vm) {
+            if (vm.getName() == name) {
+                return vm;
+            }
+            vm = vm.getParent();
+        }
+
+        return null;
+    }
+});
