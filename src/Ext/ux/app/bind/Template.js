69ebf91c287790ac8a62fbd5cc3e99f9fa917bd9
alexey <alexey@noyabrsk-dobycha.gazprom.ru>
2016-05-25 17:12:29 +0500

    Added support for prefixed Template binding, e.g. 'Selected {mymodel|selection.name}'

---
+++ src/Ext/ux/app/bind/Template.js
@@ -0,0 +1,8 @@
+﻿/**
+This override replaces tokenRe to match a token with nameDelimiter
+*/
+Ext.define('Ext.ux.app.bind.Template', {
+    override: 'Ext.app.bind.Template',
+    
+    tokenRe: /\{[!]?(?:(?:(\d+)|([a-z_][\w\-\.|]*))(?::([a-z_\.]+)(?:\(([^\)]*?)?\))?)?)\}/gi
+});
